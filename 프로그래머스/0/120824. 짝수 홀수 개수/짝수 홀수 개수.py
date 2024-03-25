93d590e8c2e2bfc12c9a1696f758661c435dfc2c
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2024-03-25 09:39:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    even_count=0
+    odd_count=0
+    
+    for num in num_list:
+        if num %2 == 0:
+            even_count+=1
+        else:
+            odd_count+=1
+    
+    answer=[even_count,odd_count]
+        
+    return answer
