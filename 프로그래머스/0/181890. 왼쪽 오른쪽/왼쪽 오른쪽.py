0776ec11b754a5b68acb789f221c2b3d78b187d2
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2025-05-06 21:27:42 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,7 @@
+def solution(str_list):
+    for i, ch in enumerate(str_list):
+        if ch == "l":
+            return str_list[:i]
+        elif ch == "r":
+            return str_list[i+1:]
+    return []
