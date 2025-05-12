9f3e4e9cbdc16ed2c74bed382f0d5edbf5070049
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2025-05-12 22:50:46 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    word_to_digit = {
+        "zero": "0", "one": "1", "two": "2", "three": "3",
+        "four": "4", "five": "5", "six": "6", "seven": "7",
+        "eight": "8", "nine": "9"
+    }
+    
+    for word, digit in word_to_digit.items():
+        numbers = numbers.replace(word, digit)
+    
+    return int(numbers)
