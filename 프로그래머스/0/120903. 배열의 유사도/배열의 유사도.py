9649dbf0f29040abb79746ae61ed1c8ebde5af96
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-19 20:14:21 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,2 @@
-def solution(s1,s2):
-    result=0
-    for word in s1:
-        if word in s2:
-            result+=1
-    
-    return result
+def solution(s1, s2):
+    return len(set(s1)&set(s2))
