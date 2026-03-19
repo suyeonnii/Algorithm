df70d185d2d98051449ae3b4a5b8f133f7b55681
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-19 14:26:59 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,9 +1,5 @@
 def solution(slice, n):
-    answer = 0
-    
-    if slice >= n:
-        answer=1
-    else:
-        answer=(n+slice-1)//slice
-        
-    return answer
+    pizza=1
+    while(pizza*slice/n<1):
+        pizza+=1
+    return pizza
