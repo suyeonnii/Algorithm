a811b830e98b58bbbb6650e199a7c8fffa3a2356
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-25 20:48:33 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,9 +1,7 @@
 def solution(num_list):
-    answer = num_list
-    
-    if num_list[-1] > num_list[-2]:
-        answer.append(num_list[-1]-num_list[-2])
+    if num_list[-1]>num_list[-2]:
+        num_list.append(num_list[-1]-num_list[-2])
     else:
-        answer.append(num_list[-1]*2)
-        
-    return answer
+        num_list.append(num_list[-1]*2)
+    
+    return num_list
