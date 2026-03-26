81c2c80a1f74169f8184af56bdd3640c95780727
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-26 16:43:52 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,9 +1,8 @@
 def solution(num_list):
-    answer = -1
-    
-    for i in range(len(num_list)):
-        if num_list[i]<0:
-            answer=i
+    for num in num_list:
+        if num <0:
+            return num_list.index(num)
             break
-        
-    return answer
+    
+    return -1
+    
