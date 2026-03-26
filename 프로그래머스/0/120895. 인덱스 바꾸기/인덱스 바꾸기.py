79404ba413a944ef398c90c6a54c55ba85b90664
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-26 21:21:01 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,6 +1,13 @@
 def solution(my_string, num1, num2):
-    chars=list(my_string)
+    answer=""
+    ch1=my_string[num1]
+    ch2= my_string[num2]
+    for i in range(len(my_string)):
+        if i==num1:
+            answer+=ch2
+        elif i==num2:
+            answer+=ch1
+        else:
+            answer+=my_string[i]
     
-    chars[num1],chars[num2]=chars[num2],chars[num1]
-    
-    return ''.join(chars)
+    return answer
