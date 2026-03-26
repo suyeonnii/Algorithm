98895de32e17794f8bed7a08bba67ab8f7c91d45
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-26 18:06:29 +0900

    [level 0] Title: 약수 구하기, Time: 0.60 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,11 +1,7 @@
 def solution(n):
-    answer = []
-    
+    result=[]
     for i in range(1,n+1):
         if n%i==0:
-            answer.append(i);
-            i=+1;
-        else:
-            i=+1;
-        
-    return answer
+            result.append(i)
+            
+    return result
