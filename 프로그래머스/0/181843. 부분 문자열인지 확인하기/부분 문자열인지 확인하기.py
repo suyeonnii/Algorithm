1b92a4f4361c7190ec7c7ac859c69393fca2b21f
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-26 18:05:06 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,8 +1,2 @@
 def solution(my_string, target):
-    answer = 0
-    
-    if target in my_string:
-        answer=1
-    else:
-        answer=0
-    return answer
+    return 1 if target in my_string else 0
