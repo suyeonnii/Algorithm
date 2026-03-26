15f03fa59d6364fd0dc22086de055976ae2a8e95
Suyeon Park <129092210+suyeonnii@users.noreply.github.com>
2026-03-26 17:56:32 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,5 +1,8 @@
 def solution(my_string, is_suffix):
-    if my_string.endswith(is_suffix):
-        return 1
-    else:
+    if len(my_string)<len(is_suffix):
         return 0
+    
+    for i in range(1,len(is_suffix)+1):
+        if my_string[-i]!=is_suffix[-i]:
+            return 0
+    return 1
